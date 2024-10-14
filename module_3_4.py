e7f9076b67bab3945e83bd373607634e4af74ab2
DimaE-g-o-r <egorovkog@mail.ru>
2024-10-14 20:19:14 +0500

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -1,10 +1,11 @@
-def single_root_words(*other_words, root_words = 'вор'):
+def single_root_words(root_words, *other_words):
     same_words = []
     for i in other_words:
         if root_words.lower() in i.lower():
             same_words.append(i)
-
+        if i.lower() in root_words.lower():
+            same_words.append(i)
     return print(same_words)
 
-single_root_words( 'воротник', 'своровать', 'воровство', 'круговорот', 'воришка')
-single_root_words('вокруг', 'кружева', 'Михаил Круг', 'круговорот', 'крутой', root_words = 'круг')
+single_root_words( 'rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
